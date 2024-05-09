1e1fada5bb734c2c237605294842a65afeae69b6
Bin2y <37493119+Bin2y@users.noreply.github.com>
2024-05-09 17:55:46 +0900

    Feat : PlayerInputMove
    Input Action& event로 플레이어가 움직인다

---
+++ Assets/Scripts/Behaviour/TopDownMovement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    [SerializeField]
+    private TopDownController controller;
+    private Rigidbody2D rigidbody;
+
+    private Vector2 movementDirection = Vector2.zero;
+
+    private void Awake()
+    {
+        controller = GetComponent<TopDownController>();
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement(movementDirection);
+    }
+
+
+    private void Move(Vector2 direction)
+    {
+        movementDirection = direction;
+    }
+    private void ApplyMovement(Vector2 direction)
+    {
+        direction = direction * 5;
+        rigidbody.velocity = direction;
+    }
+}
---
+++ Assets/Scripts/Controller/PlayerInputController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : TopDownController
+{
+    private Camera camera;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+    }
+
+    public void OnLook(InputValue value)
+    {
+        Vector2 newAim = value.Get<Vector2>();
+        Vector2 worldPos = camera.WorldToScreenPoint(newAim);
+        //마우스랑 스크립트가 붙을 객체와의 방향벡터
+        newAim = (worldPos - (Vector2)transform.position).normalized;
+
+        if(newAim.magnitude >=.9f)
+        {
+            CallLookEvent(newAim);
+        }
+    }
+}
