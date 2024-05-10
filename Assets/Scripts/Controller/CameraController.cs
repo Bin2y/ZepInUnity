b70a8f03943c1960beec1637699cd83feafdd0af
Bin2y <37493119+Bin2y@users.noreply.github.com>
2024-05-10 09:53:56 +0900

    Feat : Penguin Animations & Camera Move
    펭귄 애니메이션과
    플레이어 따라움직이는 카메라 기능구현

---
+++ Assets/Scripts/Controller/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraController : TopDownController
+{
+    [SerializeField]
+    private float cameraSpeed;
+    [SerializeField]
+    private Transform player;
+
+    private void Start()
+    {
+        cameraSpeed = 100f;
+    }
+    private void Update()
+    {
+        Vector3 dir = player.position - transform.position;
+        Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, dir.y * cameraSpeed * Time.deltaTime, 0.0f);
+        transform.Translate(moveVector);
+    }
+}
