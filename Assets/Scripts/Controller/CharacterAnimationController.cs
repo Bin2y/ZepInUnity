a7c99ba1b9f6b251b076e4384e91435300bf7939
Bin2y <37493119+Bin2y@users.noreply.github.com>
2024-05-10 10:41:09 +0900

    Feat : Character Walking Animation

---
+++ Assets/Scripts/Controller/CharacterAnimationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class CharacterAnimationController : AnimationController
+{
+    private static readonly int IsWalking = Animator.StringToHash("isWalking");
+
+    private float magnitudeThreshold = 0.5f;
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += MoveAnimation;
+    }
+
+    private void MoveAnimation(Vector2 obj)
+    {
+        animator.SetBool(IsWalking, obj.magnitude > magnitudeThreshold);
+    }
+}
