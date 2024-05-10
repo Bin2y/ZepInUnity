c7f7f5919f231724d2eed7110da21b06c05d0085
Bin2y <37493119+Bin2y@users.noreply.github.com>
2024-05-10 20:07:16 +0900

    Make : Base Player UI

---
+++ Assets/Scripts/UI/PlayerUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour
+{
+    public Text playerNameText;
+
+    private void Start()
+    {
+        //최초로 한번 이름 등록
+        SetPlayerNameText();
+    }
+    private void SetPlayerNameText()
+    {
+        if (DataManager.instance.playerName != null)
+        {
+            playerNameText.text = DataManager.instance.playerName;
+        }
+    }
+}
